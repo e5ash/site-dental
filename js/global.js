8b520703be4cd35ec54b5036504ef5692a59fdf5
e5ash <e5ash.bro@gmail.com>
2017-06-13 03:01:00 +0300

    Upload

---
+++ js/global.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+	autoplay: true,
+	autoplaySpeed: 4000
+});
+
+// $(document).ready(function($) {
+	// var stoma = $('.services__stoma'),
+	// 	stomaH = stoma.height(),
+	// 	cosmetic = $('.services__cosmetic');
+	// 	cosmeticH = cosmetic.height();
+
+	// if (stomaH>cosmeticH) {
+	// 	cosmetic.height(stomaH);
+	// } else {
+	// 	stoma.height(cosmeticH);
+	// }
+// });
